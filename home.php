cda85acd814b04228a171b9514b85829fd29f101
Chitipatgul Rungrueang <35485372+chitipatgul@users.noreply.github.com>
2026-01-15 14:14:01 +0700

    Change background color

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-<body bgcolor="#CC9900">
+<body bgcolor="#00cc99">
 	<table width="1238" height="530" border="0">
 		<tr>
 			<td width="23" rowspan="2">&nbsp;</td>
@@ -31,3 +31,4 @@
 		</tr>
 	</table>
 <body>
+
